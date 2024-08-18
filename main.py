404e4a3d8031ee17f0280b930e52b4fdcfb14db5
Ashadi De Silva <107479345+ashadiiii@users.noreply.github.com>
2024-08-18 10:36:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ st.title("Screen 1")
 with st.form("Load Data"):
     # PDF file loader for Screen 1
     
-    # TASK 2: VectorStore Information
     if load_documents(): # Load if there are existing db/ and files
         read_from_chroma()      # Instantiate and Read from Chroma  
         ask_for_more_documents  # Ask if there are more documents to ingest
@@ -19,4 +18,3 @@ with st.form("Load Data"):
     st.form_submit_button("Submit")
 
 ####### SCREEN 2
-# Task 3: 
